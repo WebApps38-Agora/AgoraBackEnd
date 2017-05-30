7664e3e996ff60dff86b186cfed8474b0a460019
Norbert Podsadowski <norbo11@googlemail.com>
2017-05-30 19:40:24 +0100

    Added News API wrapper

---
+++ topics/newsapi/newsapi.py
@@ -0,0 +1,26 @@
+import requests
+
+NEWS_API_KEY = os.environ['NEWS_API_KEY']
+NEWS_API_ARTICLES = 'https://newsapi.org/v1/articles'
+NEWS_API_SOURCES = 'https://newsapi.org/v1/sources'
+
+def get_articles(source, sortBy):
+    params = {
+        'source': source,
+        'sortBy': sortBy,
+        'apiKey': NEWS_API_KEY
+    }
+
+    response = requests.get(NEWS_API_ARTICLES, params=params).json()
+    return response['articles'] if response['status'] == 'ok' else return []
+
+def get_sources(language='en'):
+    params = {
+        'language': language
+    }
+
+    response = requests.get(NEWS_API_SOURCES, params=params)
+    return response.json()['sources']
+
+
+    
