1a080f5fb65ac2c4a27c9b36be92e1896ae1f09a
Vincenzo Pandolfo <vp715@ic.ac.uk>
2017-05-30 12:34:15 +0100

    Django set up to use Postgres

--- agora/settings.py
+++ agora/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = 'agora.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['PGDATABASE'],
+        'USER': os.environ['PGUSER'],
+        'PASSWORD': os.environ['PGPASSWORD'],
+        'HOST': os.environ['PGHOST'],
+        'PORT': os.environ['PGPORT'],
+    },
 }
 
 
