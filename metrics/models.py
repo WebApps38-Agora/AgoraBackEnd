abf185309d05ef9c3a672968cb8379ac28dd4f49
Vincenzo Pandolfo <pandolfovince@gmail.com>
2017-05-31 13:00:05 +0100

    Passing metric tests

--- metrics/models.py
+++ metrics/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -11,6 +12,7 @@ class Metric(models.Model):
 
     location = models.IntegerField()
     content = models.TextField()
+    content_end = models.PositiveIntegerField()
 
     REACTION_CHOICES = (
         (1, "Bias"),
@@ -21,3 +23,33 @@ class Metric(models.Model):
 
     reaction = models.IntegerField(choices=REACTION_CHOICES)
     comment = models.CharField(max_length=140)
+
+    def save(self, *args, **kwargs):
+        """
+        Check for overlapping reactions assumes content is going to be correct
+        """
+        self.content_end = self.location + len(self.content)
+        reactions = Metric.objects.filter(
+            article=self.article,
+            topic=self.topic,
+            owner=self.owner,
+            reaction=self.reaction,
+        )
+
+        overlapping_count = (
+            reactions.filter(
+                location__gte=self.location,
+                location__lt=self.content_end
+            ) |
+            reactions.filter(
+                content_end__lte=self.content_end,
+                content_end__gt=self.location
+            )
+        ).distinct().count()
+
+        if overlapping_count > 0:
+            raise ValidationError(
+                "Cannot add the same reaction twice to the same text"
+            )
+
+        return super().save(args, kwargs)
