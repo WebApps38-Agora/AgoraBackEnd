b8e81df576084ef85f8aeee505c810d40852f9fb
Norbert Podsadowski <norbo11@googlemail.com>
2017-06-01 17:41:51 +0100

    Create topics based on groupings obtained from similarity matrix

--- topics/semantic.py
+++ topics/semantic.py
@@ -1,5 +1,9 @@
 from gensim import corpora, models, similarities
-from models import Article
+from models import Article, Topic
+from collections import defaultdict
+import numpy
+
+SIMILARITY_THRESHOLD = 0.25
 
 class ArticleCorpus(corpora.TextCorpus):
     def get_texts(self):
@@ -11,30 +15,53 @@ class ArticleCorpus(corpora.TextCorpus):
 
 
 def create_article_corpus():
-    headlines = [article.headline for article in Article.objects.all()]
+    # Create a corpus using tokenized headlines
+    articles = Article.objects.all()
+    headlines = [article.headline for article in articles]
     tokenized_headlines = [tokenize(headline) for headline in headlines]
     corpus = ArticleCorpus(tokenized_headlines)
 
+    # Create a TFIDF model for the corpus
     tfidf = models.TfidfModel(corpus)
     corpus_tfidf = tfidf[corpus]
 
-    lsi = models.LsiModel(corpus, id2word=corpus.dictionary, num_topics=300)
+    # Create an LSI model based on the TFIDF model
+    lsi = models.LsiModel(corpus_tfidf, id2word=corpus.dictionary, num_topics=len(headlines))
     corpus_lsi = lsi[corpus]
 
-    index = similarities.MatrixSimilarity(lsi[corpus])
-    similarity_table = index[[to_lsi_vector(corpus, lsi, headline) for headline in headlines]]
+    # Create a similarity matrix and a table of all similarities between the headlines
+    index = similarities.MatrixSimilarity(corpus_lsi)
+    similarity_table = index[[to_lsi_vector(corpus, lsi, headline) for headline in tokenized_headlines]]
 
+    groups = defaultdict(set)
     for i, sims in enumerate(similarity_table):
         sorted_sims = sorted(enumerate(sims), key=lambda item: -item[1])
         similarity = sorted_sims[1]
         print(str(similarity[1]) + ': ' + headlines[i] + ' is most similar to ' + headlines[similarity[0]])
 
-def to_lsi_vector(corpus, lsi, headline):
-    vec_bow = corpus.dictionary.doc2bow(tokenize(headline))
+        groups[i] = set()
+        if (similarity[1] > SIMILARITY_THRESHOLD):
+            groups[i].add(similarity[0])
+            groups[similarity[0]].add(i)
+
+            groups[i].update(groups[similarity[0]])
+            groups[similarity[0]].update(groups[i])
+
+    for key, article_set in groups.iteritems():
+        a = articles[key]
+        if a.topics.count() == 0:
+            topic = Topic(title=a.headline)
+            topic.save()
+            for article in article_set:
+                a = articles[article]
+                a.topics.add(topic)
+                a.save()
+
+def to_lsi_vector(corpus, lsi, tokenized_headline):
+    vec_bow = corpus.dictionary.doc2bow(tokenized_headline)
     vec_lsi = lsi[vec_bow]
     return vec_lsi
 
 def tokenize(text):
-    #return text.split()
     stoplist = set('for a of the and to in'.split())
     return [word for word in text.lower().split() if word not in stoplist]
