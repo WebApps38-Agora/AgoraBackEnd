a5e004ea62d7521fe248205d40d11e5209181bf8
Vincenzo Pandolfo <pandolfovince@gmail.com>
2017-06-02 04:31:38 +0100

    updated deploy key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_02ff0cf6ea60_key -iv $encrypted_02ff0cf6ea60_iv -in scripts/deploy_key.enc -out deploy_key
+openssl aes-256-cbc -K $encrypted_63458b5e578d_key -iv $encrypted_63458b5e578d_iv -in scripts/deploy_key.enc -out deploy_key -d
 eval "$(ssh-agent -s)"
 chmod 600 deploy_key
 ssh-add deploy_key
-ssh $TUNNELUNAME@$TUNNEL "ssh $USERNAME@$AGORABACKEND /usr/bin/deploy.sh"
+ssh -oStrictHostKeyChecking=no $TUNNELUNAME@$TUNNEL "ssh $USERNAME@$AGORABACKEND /usr/bin/deploy.sh"
