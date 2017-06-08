b4662c550eeec43b50b795482d45701e1d450c48
Norbert Podsadowski <norbo11@googlemail.com>
2017-06-08 18:50:17 +0100

    Added Fact score test

--- agora/settings/_base.py
+++ agora/settings/_base.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     # Our apps
     'topics',
     'metrics',
+    'facts',
 ]
 
 MIDDLEWARE = [
--- facts/tests.py
+++ facts/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from facts.models import Fact, Reaction
+from topics.models import Topic
+
+class FactTest(TestCase):
+
+    def setUp(self):
+        self.t = Topic()
+        self.t.save()
+
+        self.u1 = User(username="1")
+        self.u1.save()
+
+        self.u2 = User(username="2")
+        self.u2.save()
+
+    def test_fact_score(self):
+        fact = Fact(content="Some Fact", topic=self.t, owner=self.u1)
+        fact.save()
+
+        reactions = [None]*3
+        reactions[0] = Reaction(owner=self.u2, fact=fact, is_upvote=True)
+        reactions[1] = Reaction(owner=self.u2, fact=fact, is_upvote=True)
+        reactions[2] = Reaction(owner=self.u2, fact=fact, is_upvote=False)
+        for reaction in reactions:
+            reaction.save()
+
+        self.assertEqual(fact.score, 1)
