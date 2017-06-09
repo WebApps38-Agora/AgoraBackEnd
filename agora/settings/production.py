ae58043f1e8295794a4e5f46beea81527459f191
Norbert Podsadowski <norbo11@googlemail.com>
2017-06-09 11:21:09 +0100

    No longer overwriting REST_FRAMEWORK in production settings

--- agora/settings/production.py
+++ agora/settings/production.py
@@ -19,8 +19,4 @@ DATABASES = {
     },
 }
 
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    )
-}
+REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = ['rest_framework.renderers.JSONRenderer']
