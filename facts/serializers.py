5efa0d7b66125ab184c0a009221b2d9142191e01
Vincenzo Pandolfo <pandolfovince@gmail.com>
2017-06-14 13:24:30 +0100

    Added owner profile to Facts
    Also made them ModelSerializer

--- facts/serializers.py
+++ facts/serializers.py
@@ -1,23 +1,30 @@
-from django.contrib.auth.models import User
-
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
 from facts.models import FactReaction, Fact
 
 
-class ReactionSerializer(serializers.HyperlinkedModelSerializer):
-    # TODO: remove next line when user API implemented
-    owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+class ReactionSerializer(serializers.ModelSerializer):
+    owner_profile = serializers.SlugRelatedField(
+        source="owner",
+        read_only=True,
+        slug_field="profile",
+    )
 
     class Meta:
         model = FactReaction
         fields = (
             "fact",
             "owner",
+            "owner_profile",
             "is_upvote",
         )
-        extra_kwargs = {"owner": {"write_only": True}}
+        extra_kwargs = {
+            "owner": {
+                "write_only": True,
+                "required": False,
+            }
+        }
         validators = [
             UniqueTogetherValidator(
                 queryset=FactReaction.objects.all(),
@@ -26,17 +33,26 @@ class ReactionSerializer(serializers.HyperlinkedModelSerializer):
         ]
 
 
-class FactSerializer(serializers.HyperlinkedModelSerializer):
-    # TODO: remove next line when user API implemented
-    owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
+class FactSerializer(serializers.ModelSerializer):
+    owner_profile = serializers.SlugRelatedField(
+        source="owner",
+        read_only=True,
+        slug_field="profile",
+    )
 
     class Meta:
         model = Fact
         fields = (
             "topic",
             "owner",
+            "owner_profile",
             "content",
             "factreaction_set",
             "score",
         )
-        extra_kwargs = {"owner": {"write_only": True}}
+        extra_kwargs = {
+            "owner": {
+                "write_only": True,
+                "required": False,
+            }
+        }
