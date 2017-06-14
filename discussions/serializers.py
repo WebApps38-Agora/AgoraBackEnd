d3e193a86befffdf2a17ba4a50d8b064f7167f46
Norbert Podsadowski <norbo11@googlemail.com>
2017-06-14 17:58:02 +0100

    Added ID to discussion serializer

--- discussions/serializers.py
+++ discussions/serializers.py
@@ -2,13 +2,14 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from discussions.models import Comment
 
-class CommentSerializer(serializers.HyperlinkedModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
     # TODO: remove next line when user API implemented
     owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
 
     class Meta:
         model = Comment
         fields = (
+            "id",
             "topic",
             "owner",
             "parent_comment",
