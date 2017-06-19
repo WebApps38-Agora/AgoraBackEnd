c7e77e30f19f0a40c7af89f10d1e270867800cab
Vincenzo Pandolfo <pandolfovince@gmail.com>
2017-06-19 13:15:35 +0100

    Added Profile to admin panel

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
-from user_profile.models import Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 
-# Register your models here.
-admin.register(Profile)
+from user_profile.models import Profile
 
 admin.site.unregister(Site)
+
+
 class SiteAdmin(admin.ModelAdmin):
     list_display = ('id', 'domain', 'name')
 
+
 admin.site.register(Site, SiteAdmin)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
